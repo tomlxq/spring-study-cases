2d06eaf7f95c98140c2ee9efb0331f788d051518
tomlxq <21429503@qq.com>
2018-04-15 06:40:07 +0800

    c

---
+++ activemq/readme.md
@@ -0,0 +1,3 @@
+ http://127.0.0.1:8080/activemq/send?msg=test%20log
+ 参考:
+ http://activemq.apache.org/getting-started.html#GettingStarted-OnWindows:
---
+++ activemq/src/main/java/com/example/LogConsumer/Consumer.java
@@ -0,0 +1,31 @@
+package com.example.LogConsumer;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+/*
+@Component
+public class LogConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogConsumer.class);
+
+    @JmsListener(destination = QueueName.LOG_QUEUE)
+    public void receivedQueue(String msg) {
+        LOGGER.info("Has received from " + QueueName.LOG_QUEUE + ", msg: " + msg);
+    }
+}*/
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Consumer {
+
+    @JmsListener(destination = "sample.queue")
+    public void receiveQueue(String text) {
+        System.out.println(text);
+    }
+
+}
